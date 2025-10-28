7a557519d6ee1e5a7b4fb96a440d938e239ff111
Postali Web Development <webdev@postali.com>
2025-10-28 13:01:01 -0400

    test

--- postali-child/header.php
+++ postali-child/header.php
@@ -41,6 +41,8 @@ endif;
 
 <body <?php body_class(); ?>>
 
+	<!-- GITHUB PUSH TEST -->
+
 	<!-- Google Tag Manager (noscript) -->
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZLKWHL"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
